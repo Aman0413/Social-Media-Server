f29015d9593729c9e724baeaf644c394d1a4fa31
Aman Verma <aman@Amans-MacBook-Air.local>
2023-03-19 19:03:30 +0530

    change

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.use(morgan("common"));
 app.use(cookieParser());
 let origin = "http://localhost:3000";
 console.log("here env", process.env.NODE_ENV);
+
 if (process.env.NODE_ENV === "production") {
   origin = process.env.CLIENT_ORIGIN;
 }
