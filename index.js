a3c9b6e4713beb1f353932caca18b031df8f56a4
Aman Verma <aman@Amans-MacBook-Air.local>
2023-03-20 00:22:19 +0530

    add frontend origin

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(morgan("common"));
 app.use(cookieParser());
-let origin = "http://localhost:3000";
+let origin = "https://cozy-unicorn-f28a5c.netlify.app";
 console.log("here env", process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === "production") {
