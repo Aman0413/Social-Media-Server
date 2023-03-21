cbce1eda8aa11630d644195c00d043eea7ad0d32
Aman Verma <aman@Amans-MacBook-Air.local>
2023-03-21 16:11:09 +0530

    production ready

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(morgan("common"));
 app.use(cookieParser());
-let origin = process.env.CLIENT_ORIGIN;
+let origin = "http://localhost:3000";
 console.log("here env", process.env.NODE_ENV);
 
 if (process.env.NODE_ENV === "production") {
