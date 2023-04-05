c96a912fb4c5275c8eef63e0a8921b4c6467773b
Aman Verma <aman@Amans-MacBook-Air.local>
2023-04-05 21:07:16 +0530

    add forget password controller

--- routers/authRouter.js
+++ routers/authRouter.js
@@ -5,5 +5,6 @@ router.post("/signup", authController.signupController);
 router.post("/login", authController.loginController);
 router.get("/refresh", authController.refreshAccessTokenController);
 router.post("/logout", authController.logoutController);
+router.post("/forgetpassword", authController.forgetPassword);
 
 module.exports = router;
